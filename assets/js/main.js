5e54a073de47d03b1e5df68b09fc0432fd0cb886
wince <winceogola20@gmail.com>
2025-11-18 09:26:16 +0300

    icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,13 +52,28 @@
 // })
 const tabs = document.querySelectorAll('[data-tab-target]');
 const tabContent = document.querySelectorAll('[data-tab-content]');
+const welcomeMessage = document.querySelector(".welcome");
+const createMessage = document.getElementById("create");
 
 tabs.forEach(tab => {
   tab.addEventListener("click", () => {
     const target = document.querySelector(tab.dataset.tabTarget);
+
+    
     tabs.forEach(t => t.classList.remove('active'));
     tab.classList.add('active');
+
+    
     tabContent.forEach(c => c.classList.remove('active'));
     target.classList.add('active');
+
+    
+    if (tab.dataset.tabTarget === "#signup") {
+      welcomeMessage.textContent = "Join TaskMaster";
+      createMessage.textContent = "Create an account to boost your productivity";
+    } else {
+      welcomeMessage.textContent = "Welcome Back!";
+      createMessage.textContent = "Enter your details to access your account";
+    }
   });
 });
