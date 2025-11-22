a26bdcb8520186381103e075c8983d1a9d59bf41
wince <winceogola20@gmail.com>
2025-11-23 01:54:55 +0300

    dashboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,49 @@ tabs.forEach(tab => {
     }
   });
 });
+// Returns all element descendants of node that match selectors ([data-import]).
+// this returns an array like element that can be looped through
+
+function renderComponents(elements){
+
+  // loop through this array list elements
+  for (let element of elements) {
+    // get the specific attributes that we stored the path to the component/module in
+    const dataImport = element.getAttribute("data-import");
+    
+    fetch(dataImport)
+      .then((res) => {
+          if(!res.ok){
+              throw "Not found"
+          }
+        return res.text();
+      })
+      .then((component) => {
+        element.innerHTML = component;
+        loadComponentScripts(element)
+        const subComponents = element.querySelectorAll("[data-import]");
+        renderComponents(subComponents)
+      })
+      .catch(() => {
+        element.innerHTML = `<h4>Component not found</h4>`;
+      });
+  }
+}
+const componentElements = document.querySelectorAll("[data-import]");
+renderComponents(componentElements)
+
+function loadComponentScripts(element){
+    const scripts = element.querySelectorAll("script");
+    for (let script of scripts) {
+        const newScript = document.createElement('script');
+        if(script.src){
+            newScript.src = script.src;
+        }
+        if(script.textContent){
+            newScript.textContent = script.textContent;
+        }
+        script.remove()
+        
+        element.appendChild(newScript)
+    }
+}
